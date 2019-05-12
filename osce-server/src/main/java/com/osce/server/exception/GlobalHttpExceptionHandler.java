41f48a17a7a168e7de95d05554d7c38020bcc717
yangtb <yang_t_b@163.com>
2019-05-12 17:32:54 +0800

    全局异常处理

--- osce-server/src/main/java/com/osce/server/exception/GlobalHttpExceptionHandler.java
+++ osce-server/src/main/java/com/osce/server/exception/GlobalHttpExceptionHandler.java
@@ -2,6 +2,7 @@ package com.osce.server.exception;
 
 import com.osce.ResultObject;
 import com.osce.exception.RestErrorCode;
+import com.osce.exception.RestException;
 import com.sm.open.care.core.exception.BizRuntimeException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,36 +22,39 @@ import javax.servlet.http.HttpServletRequest;
 @ControllerAdvice
 public class GlobalHttpExceptionHandler {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(GlobalHttpExceptionHandler.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalHttpExceptionHandler.class);
 
-	@ResponseBody
-	@ExceptionHandler(Exception.class)
-	public ResultObject globalExceptionResolver(HttpServletRequest request, Exception e) {
-		String requestUrl = request.getServletPath();
-		if (e instanceof BizRuntimeException) {
-			BizRuntimeException bizEx = (BizRuntimeException) e;
-			return ResultObject.create(requestUrl, bizEx.getErrorCode(), bizEx.getMessage());
-		} else if (e instanceof AccessDeniedException) {
-			if (isAjaxReq(request)) {
-				return ResultObject.create(requestUrl, RestErrorCode.ACCESS_RESTRICTED_ERROR.getMessage(), "没有访问权限");
-			}
-			// 无权限异常抛到上层
-			throw new AccessDeniedException(e.getMessage());
-		} else {
-			LOGGER.info("【GlobalExceptionResolver】服务器内部错误, url:{}, msg:{}", requestUrl, e.getMessage());
-			return ResultObject.create(requestUrl, RestErrorCode.UNKNOWN_ERROR.getCode(), RestErrorCode.UNKNOWN_ERROR.getMessage());
-		}
-	}
+    @ResponseBody
+    @ExceptionHandler(Exception.class)
+    public ResultObject globalExceptionResolver(HttpServletRequest request, Exception e) {
+        String requestUrl = request.getServletPath();
+        if (e instanceof BizRuntimeException) {
+            BizRuntimeException bizEx = (BizRuntimeException) e;
+            return ResultObject.create(requestUrl, bizEx.getErrorCode(), bizEx.getMessage());
+        } else if (e instanceof RestException) {
+            RestException restException = (RestException) e;
+            return ResultObject.create(requestUrl, restException.getCode(), restException.getMessage());
+        } else if (e instanceof AccessDeniedException) {
+            if (isAjaxReq(request)) {
+                return ResultObject.create(requestUrl, RestErrorCode.ACCESS_RESTRICTED_ERROR.getMessage(), "没有访问权限");
+            }
+            // 无权限异常抛到上层
+            throw new AccessDeniedException(e.getMessage());
+        } else {
+            LOGGER.info("【GlobalExceptionResolver】服务器内部错误, url:{}, msg:{}", requestUrl, e.getMessage());
+            return ResultObject.create(requestUrl, RestErrorCode.UNKNOWN_ERROR.getCode(), RestErrorCode.UNKNOWN_ERROR.getMessage());
+        }
+    }
 
-	/**
-	 * 判断是否是ajax请求
-	 *
-	 * @param request
-	 * @return
-	 */
-	private boolean isAjaxReq(HttpServletRequest request) {
-		return request.getHeader("x-requested-with") != null
-				&& "XMLHttpRequest".equalsIgnoreCase(request.getHeader("x-requested-with"));
-	}
+    /**
+     * 判断是否是ajax请求
+     *
+     * @param request
+     * @return
+     */
+    private boolean isAjaxReq(HttpServletRequest request) {
+        return request.getHeader("x-requested-with") != null
+                && "XMLHttpRequest".equalsIgnoreCase(request.getHeader("x-requested-with"));
+    }
 
 }
