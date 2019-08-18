c822fb97462dc770ccafc1748caed6351a6ec1f8
yangtb <yang_t_b@163.com>
2019-08-18 22:23:43 +0800

    参数页面调整

--- osce-server/src/main/java/com/osce/server/portal/system/param/PfParamController.java
+++ osce-server/src/main/java/com/osce/server/portal/system/param/PfParamController.java
@@ -4,7 +4,6 @@ import com.osce.api.system.param.PfParamService;
 import com.osce.dto.system.param.ParamDto;
 import com.osce.enums.SysDicGroupEnum;
 import com.osce.result.PageResult;
-import com.osce.result.ResultFactory;
 import com.osce.server.portal.BaseController;
 import com.osce.server.utils.EnumUtil;
 import org.apache.dubbo.config.annotation.Reference;
@@ -23,7 +22,6 @@ import javax.annotation.Resource;
  * @Date 2017/10/9 11:05
  */
 @Controller
-@RequestMapping(value = "/pf/p/param")
 public class PfParamController extends BaseController {
 
     @Reference
@@ -32,14 +30,14 @@ public class PfParamController extends BaseController {
     private EnumUtil enumUtil;
 
     @PreAuthorize("hasAnyRole('ROLE_PARAM_MG','ROLE_SUPER')")
-    @RequestMapping("/page")
+    @RequestMapping("/pf/p/param/page")
     public String page(Model model) {
         model.addAttribute("modelList", enumUtil.getEnumList(SysDicGroupEnum.SYS_PARAM_BIZ_MODUAL.getCode()));
         return "pages/system/param/param";
     }
 
     @PreAuthorize("hasAnyRole('ROLE_PARAM_MG','ROLE_SUPER')")
-    @RequestMapping("/form")
+    @RequestMapping("/pf/p/param/form")
     public String form(String formType, Model model) {
         model.addAttribute("formType", formType);
         model.addAttribute("modelList", enumUtil.getEnumList(SysDicGroupEnum.SYS_PARAM_BIZ_MODUAL.getCode()));
@@ -50,7 +48,7 @@ public class PfParamController extends BaseController {
      * 获取参数列表
      */
     @PreAuthorize("hasAnyRole('ROLE_PARAM_MG','ROLE_SUPER')")
-    @RequestMapping(value = "/list")
+    @RequestMapping(value = "/pf/p/param/list")
     @ResponseBody
     public PageResult listDicGroups(ParamDto dto) {
         return pfParamService.listParams(dto);
