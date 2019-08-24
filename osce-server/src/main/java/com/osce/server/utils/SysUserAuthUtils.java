beaa0a2277dc1e03e76a607daf487b415f713af7
yangtb <yang_t_b@163.com>
2019-08-24 15:11:21 +0800

    修复bug

--- osce-server/src/main/java/com/osce/server/utils/SysUserAuthUtils.java
+++ osce-server/src/main/java/com/osce/server/utils/SysUserAuthUtils.java
@@ -1,9 +1,6 @@
 package com.osce.server.utils;
 
-import com.osce.server.security.CurrentUserUtils;
 import com.osce.server.security.SecurityContext;
-import com.osce.server.security.User;
-import com.sm.open.care.core.enums.YesOrNoNum;
 
 public class SysUserAuthUtils {
 
@@ -13,8 +10,7 @@ public class SysUserAuthUtils {
      * @return
      */
     public static boolean isPlatOrSuper() {
-        User user = CurrentUserUtils.getCurrentUser();
-        if (SecurityContext.hasRole("ROLE_SUPER") || user.getFgPlat().equals(YesOrNoNum.YES.getCode())) {
+        if (SecurityContext.hasRole("ROLE_SUPER")) {
             return true;
         } else {
             return false;
